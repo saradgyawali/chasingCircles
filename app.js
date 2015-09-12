148f9bc8a0b3ec5c95a7f81c76463a3d1d648aaf
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-12 13:12:03 -0700

    clean code

--- app.js
+++ app.js
@@ -1,103 +1,103 @@
 $(document).ready(function(){
 	$(".circle1").mouseenter(function(){
 		$(".circle1").css("background-color", "blue")
-	})
+	});
 	$(".circle2").mouseenter(function(){
 		$(".circle2").css("background-color", "red")
-	})
+	});
 	$(".circle3").mouseenter(function(){
 		$(".circle3").css("background-color", "yellow")
-	})
+	});
 	$(".circle4").mouseenter(function(){
 		$(".circle4").css("background-color", "green")
-	})
+	});
 	$(".circle5").mouseenter(function(){
 		$(".circle5").css("background-color", "blue")
-	})
+	});
 	$(".circle6").mouseenter(function(){
 		$(".circle6").css("background-color", "red")
-	})
+	});
 	$(".circle7").mouseenter(function(){
 		$(".circle7").css("background-color", "yellow")
-	})
+	});
 	$(".circle8").mouseenter(function(){
 		$(".circle8").css("background-color", "green")
-	})
+	});
 	$(".circle9").mouseenter(function(){
 		$(".circle9").css("background-color", "blue")
-	})
+	});
 	$(".circle10").mouseenter(function(){
 		$(".circle10").css("background-color", "red")
-	})
+	});
 	$(".circle11").mouseenter(function(){
 		$(".circle11").css("background-color", "yellow")
-	})
+	});
 	$(".circle12").mouseenter(function(){
 		$(".circle12").css("background-color", "green")
-	})
+	});
 	$(".circle13").mouseenter(function(){
 		$(".circle13").css("background-color", "blue")
-	})
+	});
 	$(".circle14").mouseenter(function(){
 		$(".circle14").css("background-color", "red")
-	})
+	});
 	$(".circle15").mouseenter(function(){
 		$(".circle15").css("background-color", "yellow")
-	})
+	});
 	$(".circle16").mouseenter(function(){
 		$(".circle16").css("background-color", "green")
-	})
+	});
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 	$(".circle1").mouseleave(function(){
 		$(".circle1").css("background-color", "red")
-	})
+	});
 	$(".circle2").mouseleave(function(){
 		$(".circle2").css("background-color", "yellow")
-	})
+	});
 	$(".circle3").mouseleave(function(){
 		$(".circle3").css("background-color", "green")
-	})
+	});
 	$(".circle4").mouseleave(function(){
 		$(".circle4").css("background-color", "blue")
-	})
+	});
 	$(".circle5").mouseleave(function(){
 		$(".circle5").css("background-color", "yellow")
-	})
+	});
 	$(".circle6").mouseleave(function(){
 		$(".circle6").css("background-color", "green")
-	})
+	});
 	$(".circle7").mouseleave(function(){
 		$(".circle7").css("background-color", "blue")
-	})
+	});
 	$(".circle8").mouseleave(function(){
 		$(".circle8").css("background-color", "red")
-	})
+	});
 	$(".circle9").mouseleave(function(){
 		$(".circle9").css("background-color", "green")
-	})
+	});
 	$(".circle10").mouseleave(function(){
 		$(".circle10").css("background-color", "blue")
-	})
+	});
 	$(".circle11").mouseleave(function(){
 		$(".circle11").css("background-color", "green")
-	})
+	});
 	$(".circle12").mouseleave(function(){
 		$(".circle12").css("background-color", "yellow")
-	})
+	});
 	$(".circle13").mouseleave(function(){
 		$(".circle13").css("background-color", "green")
-	})
+	});
 	$(".circle14").mouseleave(function(){
 		$(".circle14").css("background-color", "yellow")
-	})
+	});
 	$(".circle15").mouseleave(function(){
 		$(".circle15").css("background-color", "red")
-	})
+	});
 	$(".circle16").mouseleave(function(){
 		$(".circle16").css("background-color", "blue")
-	})
+	});
 
 
-})
+});
